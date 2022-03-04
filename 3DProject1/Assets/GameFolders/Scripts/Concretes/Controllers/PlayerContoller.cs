8baf6deb1bfaf760bfdf8780570e793112dc35a4
Kulac12 <kulacedanur460@gmail.com>
2022-03-05 02:46:21 +0300

    player companent

---
+++ 3DProject1/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerContoller.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controllers 
+{
+
+        
+        public class PlayerContoller : MonoBehaviour
+        {
+            
+        }
+
+
+
+
+}
